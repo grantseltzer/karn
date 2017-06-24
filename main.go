1ac7b2c1cab99b711324f10a7f671f0113b2bb3b
grantseltzer <grant@capsule8.com>
2017-06-24 15:23:03 -0400

    start of cli functionality
    Signed-off-by: grantseltzer <grant@capsule8.com>

--- main.go
+++ main.go
@@ -1,22 +1,49 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
+	"io"
 	"log"
+	"os"
 
 	parse "github.com/GrantSeltzer/karn/parse"
+	"github.com/spf13/cobra"
 )
 
 const declarationDirectory = "declarations/"
 
-func main() {
+func newRootCmd(rguments []string, out io.Writer) *cobra.Command {
 
-	x, err := parse.BuildSeccompConfig("prof.toml", declarationDirectory)
-	if err != nil {
-		log.Fatal(err)
+	var karnRootCommand = &cobra.Command{
+		Use:   "capsule8 [OPTIONS] <Profile_Name.toml>",
+		Short: "command-line client to capsule8 API",
+		Run: func(cmd *cobra.Command, args []string) {
+
+			if len(args) == 0 {
+				fmt.Fprintf(os.Stderr, "not enough arguments, please specify a profile location")
+				fmt.Fprintf(os.Stderr, "Use `karn --help` for more info\n")
+				os.Exit(1)
+			}
+
+			x, err := parse.BuildSeccompConfig(args[0], declarationDirectory)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			out.Write([]byte(fmt.Sprintf("%+v\n", x)))
+		},
 	}
 
-	asJson, _ := json.Marshal(x)
-	fmt.Println(string(asJson))
+	// Uncomment to add flags/subcommands
+	// rootFlags := karnRootCommand.PersistentFlags()
+	// karnRootCommand.AddCommand()
+
+	return karnRootCommand
+}
+
+func main() {
+	k := newRootCmd(os.Args, os.Stdout)
+	if err := k.Execute(); err != nil {
+		os.Exit(1)
+	}
 }
